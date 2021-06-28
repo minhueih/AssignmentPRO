549b4fbf77de34a55c4848028664830b3820294a
MinnminX7 <86579768+MinnminX7@users.noreply.github.com>
2021-06-28 14:07:13 +0700

    Add files via upload

---
+++ AssignmentPRO/src/IOFile.java
@@ -0,0 +1,37 @@
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class IOFile {
+    //doc file
+    public static <T> List<T> read(String file){
+        List<T> list=new ArrayList<>();
+        try{
+            ObjectInputStream o=new ObjectInputStream(
+                    new FileInputStream(file));
+            list=(List<T>)o.readObject();
+            o.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }catch(ClassNotFoundException e){
+            System.out.println(e);
+        }
+        return list;
+    }
+    //ghi vao file
+    public static<T> void write(String file,List<T> arr){
+        try{
+            ObjectOutputStream o=new ObjectOutputStream(new FileOutputStream(file));
+            o.writeObject(arr);
+            o.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
