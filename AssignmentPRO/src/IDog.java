fb1e989ba161be52460f77bd8dbfc38a10e0536c
baro-jury <84394521+baro-jury@users.noreply.github.com>
2021-06-28 10:09:04 +0700

    Add files via upload

---
+++ AssignmentPRO/src/IDog.java
@@ -0,0 +1,16 @@
+
+public interface IDog {
+    public void listPetDogs();
+    public void addPetDog();
+    public void removePetDog(String id);
+    public void savePetDogs();
+    public void listDogs();
+    public void addDog();
+    public void removeDog(String id);
+    public void updateDog(String id);
+    public void saveDogs();
+    public void listAscending(String name);
+    public void listAscending(double price);
+    public void search(String color);
+    public void search(double price);
+}
