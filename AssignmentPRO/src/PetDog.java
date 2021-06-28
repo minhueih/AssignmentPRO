c22135faaffc5277fb3613223200a048acc68a1d
MinnminX7 <86579768+MinnminX7@users.noreply.github.com>
2021-06-28 13:48:49 +0700

    Update PetDog.java

--- AssignmentPRO/src/PetDog.java
+++ AssignmentPRO/src/PetDog.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,6 +11,45 @@
  *
  * @author Admin
  */
-public class PetDog {
+public class PetDog implements IPet<PetDogData> {
+    
+    private List<PetDogData> _list;
+    
+    @Override
+    public void listAll() {
+        _list.forEach((_dog) -> {
+            System.out.println(_dog.toString());
+        });
+    }
+
+    @Override
+    public void addPet(PetDogData petdog) {
+        for (PetDogData d : _list) {
+            if (d.getID() == petdog.getID()) {
+                System.out.println("Dog already existed!");
+                return;
+            }
+        }
+        if (petdog == null || _list.contains(petdog)) {
+            System.out.println("Dog already existed!");
+            return;
+        }
+        _list.add(petdog);
+    }
+
+    @Override
+    public void removePet(String id) {
+        for (PetDogData d : _list) {
+            if (d.getID() == Integer.parseInt(id)) {
+                _list.remove(d);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void saveFile() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
